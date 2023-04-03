1b9ac0dd454202c001011848d1e64c53bbf357fd
WinKyaw <winkyaw1174@gmail.com>
2023-04-03 17:04:12 +0630

    Add project files.

---
+++ Models/UserMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Chatty.Models
+{
+    public class UserMessage
+    {
+        public string? Username{ get; set; }
+        public string? Message{ get; set; }
+        public bool CurrentUser{ get; set; }
+        public DateTime DateSent{ get; set; }
+
+    }
+}
